cf8f324494850ebc1dc5d31fc27a2e9add2c5c6f
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-25 17:38:12 +0900

    コメント追加

--- lib/file_storage_server.rb
+++ lib/file_storage_server.rb
@@ -35,7 +35,29 @@ class FileStorageServer < Sample::FileStorage::Service
   end
 end
 
-s = GRPC::RpcServer.new
-s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
-s.handle(Server)
-s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
+__END__
+
+require_relative './lib/s3'
+
+method = ARGV[0]
+file_name_or_path = ARGV[1]
+
+def upload(file_path)
+  file      = File.open(file_path)
+  file_name = File.basename(file.path)
+  file_blob = file.read
+  S3.upload(file_name, file_blob)
+
+  puts "Upload 完了"
+end
+
+def download(file_name)
+  obj = S3.download(file_name)
+  File.open("#{Dir.home}/Desktop/test_#{file_name}", "w") do |f|
+    f.write(obj.body.read)
+  end
+
+  puts "Download 完了"
+end
+
+send(method, file_name_or_path)
