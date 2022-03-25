4cf7fbc9bdf3cc02cda28ed2bc7f60d02aa12e2a
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-26 08:51:39 +0900

    不要なコメントを削除

--- lib/file_storage_server.rb
+++ lib/file_storage_server.rb
@@ -35,30 +35,3 @@ class FileStorageServer < Sample::FileStorage::Service
     response
   end
 end
-
-__END__
-
-require_relative './lib/s3'
-
-method = ARGV[0]
-file_name_or_path = ARGV[1]
-
-def upload(file_path)
-  file      = File.open(file_path)
-  file_name = File.basename(file.path)
-  file_blob = file.read
-  S3.upload(file_name, file_blob)
-
-  puts "Upload 完了"
-end
-
-def download(file_name)
-  obj = S3.download(file_name)
-  File.open("#{Dir.home}/Desktop/test_#{file_name}", "w") do |f|
-    f.write(obj.body.read)
-  end
-
-  puts "Download 完了"
-end
-
-send(method, file_name_or_path)
