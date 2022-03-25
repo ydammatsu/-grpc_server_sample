e5ba203e15372ed31ec6d9b5e3c57a2a6b068362
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-25 15:18:12 +0900

    add S3Handler

---
+++ lib/s3handler.rb
@@ -0,0 +1,39 @@
+require 'aws-sdk-s3'
+
+class S3Handler
+  BUCKET_NAME = 'yurusuta-sample1'
+
+  def self.upload(file_name, file_blob)
+    client.put_object(
+      bucket: BUCKET_NAME,
+      key: file_name,
+      body: file_blob,
+      metadata: {}
+    )
+  rescue Aws::Errors::ServiceError => e
+    puts "Couldn't upload file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
+    false
+  end
+
+  def self.download(file_name)
+    obj = client.get_object(bucket: BUCKET_NAME, key: file_name)
+    obj
+  rescue Aws::Errors::ServiceError => e
+    puts "Couldn't download file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
+    false
+  end
+
+  private
+
+  def self.client
+    @client ||= Aws::S3::Client.new(
+      region: 'ap-northeast-1',
+      credentials: Aws::SSOCredentials.new(
+        sso_account_id: '348858295373',
+        sso_role_name: 'AdministratorAccess',
+        sso_region: 'ap-northeast-1',
+        sso_start_url: 'https://d-95671c748d.awsapps.com/start'
+      )
+    )
+  end
+end
