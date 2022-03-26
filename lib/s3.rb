c750e454d72249a56d0f69217c0289d7279d56eb
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-26 09:03:09 +0900

    fix

--- lib/s3.rb
+++ lib/s3.rb
@@ -6,39 +6,43 @@ require 'aws-sdk-s3'
 class S3
   BUCKET_NAME = 'yurusuta-sample1'
 
-  def self.upload(file_name, file_blob)
-    client.put_object(
-      bucket: BUCKET_NAME,
-      key: file_name,
-      body: file_blob,
-      metadata: {}
-    )
-  rescue Aws::Errors::ServiceError => e
-    puts "Couldn't upload file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
-    false
-  end
+  class << self
+    def upload(file_name, file_blob)
+      client.put_object(
+        bucket: BUCKET_NAME,
+        key: file_name,
+        body: file_blob,
+        metadata: {}
+      )
+    rescue Aws::Errors::ServiceError => e
+      puts "Couldn't upload file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
+      false
+    end
 
-  def self.download(file_name)
-    obj = client.get_object(bucket: BUCKET_NAME, key: file_name)
-    obj.body.read
-  rescue Aws::Errors::ServiceError => e
-    puts "Couldn't download file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
-    false
-  end
+    def download(file_name)
+      obj = client.get_object(bucket: BUCKET_NAME, key: file_name)
+      obj.body.read
+    rescue Aws::Errors::ServiceError => e
+      puts "Couldn't download file #{file_name} to #{BUCKET_NAME}. Here's why: #{e.message}"
+      false
+    end
 
-  def self.client
-    @client ||= Aws::S3::Client.new(
-      region: 'ap-northeast-1',
-      credentials: sso_credentials
-    )
-  end
+    private
 
-  def self.sso_credentials
-    @sso_credentials = Aws::SSOCredentials.new(
-      sso_account_id: ENV['SANDBOX_KYOTO_ACCOUNT_ID'],
-      sso_role_name: ENV['SANDBOX_KYOTO_ROLE'],
-      sso_region: 'ap-northeast-1',
-      sso_start_url: ENV['MF_SSO_START_URL']
-    )
+    def client
+      @client ||= Aws::S3::Client.new(
+        region: 'ap-northeast-1',
+        credentials: sso_credentials
+      )
+    end
+
+    def sso_credentials
+      @sso_credentials = Aws::SSOCredentials.new(
+        sso_account_id: ENV['SANDBOX_KYOTO_ACCOUNT_ID'],
+        sso_role_name: ENV['SANDBOX_KYOTO_ROLE'],
+        sso_region: 'ap-northeast-1',
+        sso_start_url: ENV['MF_SSO_START_URL']
+      )
+    end
   end
 end
