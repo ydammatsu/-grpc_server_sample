698ec228a90db006bee0780c37f2b3b193a8482d
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2022-03-26 14:51:26 +0900

    修正

--- bin/server.rb
+++ bin/server.rb
@@ -7,7 +7,9 @@ require './lib/file_storage_server'
 
 s = GRPC::RpcServer.new
 s.add_http2_port('0.0.0.0:50051', :this_port_is_insecure)
-s.handle(FileStorageServer)
-s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
 
-puts "Start gRPC server."
+s.handle(FileStorageServer)
+puts " Start gRPC server."
+
+s.run_till_terminated_or_interrupted([1, 'int', 'SIGQUIT'])
+puts " Stop gRPC server."
